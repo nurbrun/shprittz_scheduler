601bfc2de4f19d6d867ba12e6a2947a7b95f5f96
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-03-31 17:23:12 -0400

    configured production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,12 @@ ShprittzScheduler::Application.configure do
   config.action_mailer.default_url_options = { :host => ENV["DOMAIN"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV["SMTP_SERVER"],
-    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    port: ENV["SMTP_PORT"].to_i,
-    domain: ENV["MAILER_DOMAIN"],
-    authentication: "plain",
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: ENV["SMTP_USER"],
-    password: ENV["SMTP_PWD"]
+    user_name: 'shprittzmailer@gmail.com',
+    password: 'asdqwe123qweasd'
   }
 end
--- db/migrate/20150324163725_add_appointment_id_to_user_again.rb
+++ db/migrate/20150324163725_add_appointment_id_to_user_again.rb
@@ -1,6 +1,7 @@
 class AddAppointmentIdToUserAgain < ActiveRecord::Migration
   def change
     self.up
+      remove_column :users, :appointment_id
       add_column :users, :appointment_id, :integer
   end
 end
