ff4e8922d7705e252d546b1393f1e0d18f7f8531
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-04-07 21:57:15 -0400

    small steps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ ShprittzScheduler::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Mailer
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'afternoon-brook-2239.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
