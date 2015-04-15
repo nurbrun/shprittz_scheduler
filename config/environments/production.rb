ed37fd649be6d62ca4336fbfd2f9cdd1b62751d1
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-04-14 23:55:57 -0400

    switched some production email settings to try to get the damn thing to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,18 +78,33 @@ ShprittzScheduler::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'afternoon-brook-2239.herokuapp.com'}
   # Mailer
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'afternoon-brook-2239.herokuapp.com', :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['EMAIL'],
-    :password       => ENV['PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["EMAIL"],
+  password: ENV["PASSWORD"]
+}
+  
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.gmail.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['EMAIL'],
+  #   :password       => ENV['PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
+
+
 end
